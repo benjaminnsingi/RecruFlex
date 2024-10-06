b9e24bd00694d2d73657133c952df16003d76cbc
benjamin nsingi <benjaminnsingi@users.noreply.github.com>
2024-10-06 18:38:33 +0200

    ✨added functionality to the service for user creation and deletion

--- src/Application/UseCase/UserService.php
+++ src/Application/UseCase/UserService.php
@@ -2,7 +2,39 @@
 
 namespace UseCase;
 
-class UserService
-{
+use App\Application\Dto\UserDto;
+use App\Domain\Entity\User;
+use App\Domain\Repository\UserRepositoryInterface;
 
+final class UserService
+{
+   public function __construct(
+      private readonly UserRepositoryInterface $userRepository,
+   ){
+   }
+
+   public function createUser(UserDto $userDto):void
+   {
+       $user = new User();
+       $user->setLastname($userDto->lastname);
+       $user->setFirstname($userDto->firstname);
+       $user->setEmail($userDto->email);
+       $user->setRoles($userDto->roles);
+
+       $this->userRepository->save($user);
+   }
+
+   public function deleteUser(int $id): bool
+   {
+      $user = $this->userRepository->find($id);
+
+      if (!$user) {
+          return false;
+      }
+
+      $user->setIsDeleted(true);
+      $this->userRepository->save($user);
+
+      return true;
+   }
 }
